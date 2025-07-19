ff20f782373667c1d3403ccb49d337b652d9f898
akkc01 <amitkkc01@gmail.com>
2025-07-19 21:03:27 +0530

    rg added1113

--- ENV/prod/terraform.tfvars
+++ ENV/prod/terraform.tfvars
@@ -4,3 +4,6 @@ subscription_id = "c316f505-7597-4175-b5db-d2949009d506"
 
 resource_group_name1 = "centralindia_prod_rg002"
 location1 = "centralindia"
+
+resource_group_name2 = "centralindia_prod_rg003"
+location2 = "centralindia"
--- ENV/prod/variables.tf
+++ ENV/prod/variables.tf
@@ -27,3 +27,15 @@ variable "location1" {
   type        = string
  
 }
+
+variable "resource_group_name2" {
+  description = "The name of the resource group to create."
+  type        = string
+  
+}
+
+variable "location2" {
+  description = "The Azure location where the resource group will be created."
+  type        = string
+ 
+}
