6403c16650d4fdc77d2dabc471ef54208b23cae4
akkc01 <amitkkc01@gmail.com>
2025-07-19 19:00:26 +0530

    rg added1

--- modules/azurerm_resource_group/variables.tf
+++ modules/azurerm_resource_group/variables.tf
@@ -9,3 +9,15 @@ variable "location" {
   type        = string
  
 }
+
+variable "resource_group_name1" {
+  description = "The name of the resource group to create."
+  type        = string
+  
+}
+
+variable "location1" {
+  description = "The Azure location where the resource group will be created."
+  type        = string
+ 
+}
